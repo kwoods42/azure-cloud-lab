965b1515a4b9d4b5b8a592fb60b8a25f2d3c98e3
Kevin Woods <kev.woods42@gmail.com>
2026-09-07 13:22:29 -0400

    Phase 3.3: Terraform full VNet + VM stack (Windows + Linux)

--- phase3-terraform/main.tf
+++ phase3-terraform/main.tf
@@ -8,13 +8,175 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
+
 resource "azurerm_resource_group" "lab" {
   name     = "rg-lab-terraform"
   location = "East US"
 }
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-lab-terraform"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  address_space       = ["10.20.0.0/16"]
+}
+
+resource "azurerm_subnet" "servers" {
+  name                 = "snet-servers"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.20.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "lab" {
+  name                = "nsg-lab-servers"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = var.my_ip
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.my_ip
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "dc01" {
+  name                = "pip-lab-dc01"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "dc01" {
+  name                = "nic-lab-dc01"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.servers.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.dc01.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "dc01" {
+  network_interface_id      = azurerm_network_interface.dc01.id
+  network_security_group_id = azurerm_network_security_group.lab.id
+}
+
+resource "azurerm_windows_virtual_machine" "dc01" {
+  name                = "vm-lab-dc01-tf"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  size                = "Standard_D2s_v7"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [azurerm_network_interface.dc01.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-g2"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_public_ip" "lx01" {
+  name                = "pip-lab-lx01"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "lx01" {
+  name                = "nic-lab-lx01"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.servers.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.lx01.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "lx01" {
+  network_interface_id      = azurerm_network_interface.lx01.id
+  network_security_group_id = azurerm_network_security_group.lab.id
+}
+
+resource "azurerm_linux_virtual_machine" "lx01" {
+  name                = "vm-lab-lx01-tf"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  size                = "Standard_D2s_v7"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [azurerm_network_interface.lx01.id]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+output "dc01_public_ip" {
+  value = azurerm_public_ip.dc01.ip_address
+}
+
+output "lx01_public_ip" {
+  value = azurerm_public_ip.lx01.ip_address
+}
+
+
+
