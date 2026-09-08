a689677e0abe760239c5790e179ba4f0931997d5
Kevin Woods <kev.woods42@gmail.com>
2026-09-08 11:31:21 -0400

    Phase 6.3: Defender for Cloud, Log Analytics, Azure Monitor alert; clean up variables

--- phase3-terraform/variables.tf
+++ phase3-terraform/variables.tf
@@ -1,6 +1,13 @@
 variable "subscription_id" {}
 variable "client_id" {}
 variable "tenant_id" {}
-variable "client_secret" {
-  sensitive = true
+
+variable "admin_username" {
+  description = "Admin username for both VMs"
+  default     = "azureadmin"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for Linux VM"
+  sensitive   = true
 }
